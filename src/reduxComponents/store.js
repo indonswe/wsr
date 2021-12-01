1ce22a146f0144e6971235b268f6314b68725047
indonswe <afc2002afc@hotmail.com>
2021-12-01 13:15:03 +0100

    Add more reducers

--- src/reduxComponents/store.js
+++ src/reduxComponents/store.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counterSlice'
+import theAmountReducer from './thePersonSlice'
 
 export default configureStore({
-  reducer: {counter: counterReducer}
+  reducer: {counter: counterReducer, amountOfPersons: theAmountReducer}
 })
